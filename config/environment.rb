b0b0b49c9446cd022b1924edb652710868c4c46a
Will Strinz <wstrinz@gmail.com>
2013-01-15 21:14:30 -0600

    email (part H) complete

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,22 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'will_paginate/array'
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "wstrinz",
+    password: "ujorcbgysumrvtqq",
+    enable_starttls_auto: true
+  }
+
+end
